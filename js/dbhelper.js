dfb8f6f07fe6fa9401dae6521601dcc3f71999a4
Biboswan Roy <biboswan98@gmail.com>
2018-06-25 20:56:35 +0530

    Feat: Create and modify functions to return path of different size type images

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,14 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`img_res/img/${restaurant.photograph}`);
+  }
+
+  /**
+   * Restaurant image URL resized to 400px
+   */
+  static imageUrlForRestaurant_400(restaurant) {
+    return (`img_res/${restaurant.photograph.split('.').join('-400.')}`);
   }
 
   /**
