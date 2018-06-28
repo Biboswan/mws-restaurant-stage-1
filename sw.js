83e9e51c44fd38ef65eeb82484f0fc68c68fb053
Biboswan Roy <biboswan98@gmail.com>
2018-06-28 23:35:38 +0530

    feat: Implement service worker for offline first experience service worker register cache static resources and images and data

---
+++ sw.js
@@ -0,0 +1,80 @@
+const staticCacheName = 'restaurantreviews-static-v1';
+const contentImgsCache = 'restaurantreviews-content-v1';
+const allCaches = [
+  staticCacheName,
+  contentImgsCache
+];
+
+self.addEventListener('install', event => {
+	self.skipWaiting();
+
+  event.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+			return cache.addAll([
+				'/index.html',
+				'/restaurant.html',
+				'/js/dbhelper.js',
+			  '/js/main.js',
+				'/js/restaurant_info.js',
+				'/build/css/styles.css',
+				'/filter.png', 
+				'/data/restaurants.json'
+			]);
+		})
+	);
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.filter(cacheName => {
+					return cacheName.startsWith('restaurantreviews-') &&
+								 !allCaches.includes(cacheName);
+				}).map(cacheName => {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+});
+
+self.addEventListener('fetch', event => {
+	const requestUrl = new URL(event.request.url);
+
+	if (requestUrl.origin === location.origin) {
+		if (requestUrl.pathname === '/') {
+			event.respondWith(caches.match('/index.html'));
+			return;
+		}
+		if (requestUrl.pathname.startsWith('/img_res/')) {
+			event.respondWith(serveImage(event.request));
+			// Cache only original (highest resolution) image
+			return;
+		}
+
+	event.respondWith(
+		caches.match(event.request,{"ignoreSearch":true}).then(response => {
+			return response || fetch(event.request);
+		})
+	);
+	}
+});
+
+const serveImage = request => {
+	let storageUrl;
+	if (request.url.includes('-400')) {
+		storageUrl = request.url.replace('-400', '').replace('s','s/img/');
+	}
+
+	return caches.open(contentImgsCache).then(cache => {
+		return cache.match(storageUrl).then(response => {
+			if (response) return response;
+
+			return fetch(request).then(networkResponse => {
+				cache.put(storageUrl, networkResponse.clone());
+				return networkResponse;
+			});
+		});
+	});
+};
