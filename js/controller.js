9e2567162ba4bf728583be1a595483be90ad4544
Biboswan Roy <biboswan98@gmail.com>
2018-09-29 19:47:01 +0530

    prettify

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,17 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then(registration => {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(
+      registration => {
         // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, err => {
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        );
+      },
+      err => {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
+      }
+    );
+  });
+}
